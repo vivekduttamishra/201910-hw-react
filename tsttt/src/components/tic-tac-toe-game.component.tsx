7673ece2751a5f27d7753d6e6766795651177ef9
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2019-10-18 13:46:20 +0530

    day3 update7

--- tsttt/src/components/tic-tac-toe-game.component.tsx
+++ tsttt/src/components/tic-tac-toe-game.component.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import TicTacToeBoard from './tic-tac-toe-board.component';
 import MessageBar from './message-bar.component';
+import {GameStatus,checkGameSatus} from '../model/tic-tac-toe.service';
 
+interface GameState{
+    cells:string[];
+    next:string;
+    message:string;
+    gameStatus:GameStatus|null;
+}
 
 class _component extends React.Component<any> {
-    state={cells:['_','_','_','_','_','_','_','_','_'], next:'O',message:'Next : O'}
+    state:GameState={
+        cells:['_','_','_','_','_','_','_','_','_'], 
+        next:'O',
+        message:'Next : O',
+        gameStatus:null
+    };
+
+    componentDidMount(){
+        
+        //this.state.gameStatus=checkGameSatus(this.state.cells);
+        //set state doesn't replace current state, it merges changes in the state
+        this.setState({gameStatus:checkGameSatus(this.state.cells)});
+    }
 
     handleCellClick=(id:number)=>{
+        console.log('handleCellClick gameStatus',this.state.gameStatus);
+        if(this.state.gameStatus && this.state.gameStatus.gameOver)
+            return ; //game is over. nothing more to do
+
         let currentCellValue= this.state.cells[id];
         if(currentCellValue!='_')
             return ;
@@ -21,16 +44,25 @@ class _component extends React.Component<any> {
 
         let cells=[... this.state.cells];
         cells[id]=current;
+
+        let gameStatus=checkGameSatus(cells);
+
         let message=`Next : ${next}`;
 
-        this.setState({cells, next,message});
+        if(gameStatus.winner){
+            message=`${gameStatus.winner} Wins`;
+        } else if(gameStatus.gameOver){
+            message='Game Ends a Draw'
+        };
+
+        this.setState({cells, next,gameStatus,message});
         
         
     }
 
 
     render = () => {
-
+        console.log('render gameStatus',this.state.gameStatus);
         return (
             <div>
                 <h1>Tic Tac Toe</h1>
